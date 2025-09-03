6972a1e942dc50e2003270b81c33c9e133abb3f4
Torbjörn Kagel <torbjoern.kagel@outlook.de>
2025-09-03 13:27:45 +0200

    delete uwibasspp.py

--- Pathfinder/uwibasspp.py
+++
@@ -1,8 +0,0 @@
-# Create the object
-class rdr():
-    def __init__(self):
-        self.rx = None
-        self.fasttime = None
-        self.range_air = None
-        self.slowtime = None
-        
