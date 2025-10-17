febf2085489072ae679fcea2bc78453b17dc41af
Torbjörn Kagel <torbjoern.kagel@outlook.de>
2025-10-17 13:48:10 +0200

    fig06 done (one large plot)

--- Pathfinder/helper_functions.py
+++ Pathfinder/helper_functions.py
@@ -423,7 +423,24 @@ def interpolateImx(x,im):
 
     return imout,xout
 
-
+def mscatter(x,y,ax=None, m=None, **kw):
+    
+    import matplotlib.markers as mmarkers
+    if not ax: ax=plt.gca()
+    sc = ax.scatter(x,y,**kw)
+    if (m is not None) and (len(m)==len(x)):
+        paths = []
+        for marker in m:
+            marker =str(marker)
+            if isinstance(marker, mmarkers.MarkerStyle):
+                marker_obj = marker
+            else:
+                marker_obj = mmarkers.MarkerStyle(marker)
+            path = marker_obj.get_path().transformed(
+                        marker_obj.get_transform())
+            paths.append(path)
+        sc.set_paths(paths)
+    return sc
 # def get_gridvalues_at_points(
 #     x, y,
 #     grid_file: str= '/Users/torka/Downloads/DTU21MSS_1min_TP.nc'
